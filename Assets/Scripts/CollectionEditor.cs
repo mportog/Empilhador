412cb2d7d9c6151054cb0a7e4a149d92da3f762a
Matheus Porto Garcia <matheuspontoporto@gmail.com>
2019-07-31 08:37:14 -0300

    initial commit

---
+++ Assets/Scripts/CollectionEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ObjectCollection))]
+public class CollectionEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Draw the default
+        base.OnInspectorGUI();
+
+        // Place the button at the bottom
+        ObjectCollection myScript = (ObjectCollection)target;
+        if (GUILayout.Button("Update Collection"))
+        {
+            myScript.UpdateCollection();
+        }
+    }
+}
+
---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+	public Transform player;
+	Vector3 offset;
+	public float damping = 1;
+
+	// Use this for initialization
+	void Start () {
+		offset = player.position - transform.position;	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = player.eulerAngles.y;
+		float angle = Mathf.LerpAngle (currentAngle, desiredAngle, damping);
+
+		Quaternion turnRotation = Quaternion.Euler (0f, angle, 0f);
+		transform.position = player.position - (turnRotation * offset);
+			
+		transform.LookAt (player);
+	}
+}
